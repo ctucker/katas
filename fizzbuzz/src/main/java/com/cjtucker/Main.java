8f36006dd9c31199a41ee9da0a52f66942951a88
Chris Tucker <chris.tucker@service-now.com>
2013-06-01 15:45:47 -0700

    Refactor: Simplify fizz calculation, removing println duplication

--- fizzbuzz/src/main/java/com/cjtucker/Main.java
+++ fizzbuzz/src/main/java/com/cjtucker/Main.java
@@ -3,12 +3,11 @@ package com.cjtucker;
 public class Main {
 	public static void main(String[] strings) {
 		for (int i = 1; i <= 100; i++) {
+			String output = String.valueOf(i);
 			if (i == 3) {
-				System.out.println("FIZZ");
+				output = "FIZZ";
 			}
-			else {
-				System.out.println(i);
-			}
+			System.out.println(output);
 		}
 	}
 }
