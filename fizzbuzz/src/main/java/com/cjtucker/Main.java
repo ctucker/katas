5f13538803efb2bd358b7dc7282a56a73057e0cf
Chris Tucker <chris.tucker@service-now.com>
2013-06-01 15:36:13 -0700

    Impl: Output line count test passing

--- fizzbuzz/src/main/java/com/cjtucker/Main.java
+++ fizzbuzz/src/main/java/com/cjtucker/Main.java
@@ -2,7 +2,7 @@ package com.cjtucker;
 
 public class Main {
 	public static void main(String[] strings) {
-		for (int i = 1; i < 100; i++) {
+		for (int i = 1; i <= 100; i++) {
 			System.out.println("1");
 		}
 	}
