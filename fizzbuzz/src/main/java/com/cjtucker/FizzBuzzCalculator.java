fddc6b6ce628a497dc6bca241b3aef7cd3e43331
Chris Tucker <chris.tucker@service-now.com>
2013-06-01 16:05:43 -0700

    Impl: Fix to FizzBuzzCalculator to pass multiple-of-3 test

--- fizzbuzz/src/main/java/com/cjtucker/FizzBuzzCalculator.java
+++ fizzbuzz/src/main/java/com/cjtucker/FizzBuzzCalculator.java
@@ -4,7 +4,7 @@ public class FizzBuzzCalculator {
 
 	public String fizzBuzz(int i) {
 		String output = String.valueOf(i);
-		if (i == 3) {
+		if (i % 3 == 0) {
 			output = "FIZZ";
 		}
 		if (i == 5) {
