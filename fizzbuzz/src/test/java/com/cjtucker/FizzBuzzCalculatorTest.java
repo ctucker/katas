839b44855283167c400b705a0bc41e016babcb3d
Chris Tucker <chris.tucker@service-now.com>
2013-06-01 16:01:45 -0700

    Test: Failing test for fizzbuzz of 5 being BUZZ

--- fizzbuzz/src/test/java/com/cjtucker/FizzBuzzCalculatorTest.java
+++ fizzbuzz/src/test/java/com/cjtucker/FizzBuzzCalculatorTest.java
@@ -1,5 +1,6 @@
 package com.cjtucker;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,10 +9,21 @@ import static org.hamcrest.Matchers.is;
 
 public class FizzBuzzCalculatorTest {
 
+	private FizzBuzzCalculator fizzBuzzCalculator;
+
+	@Before
+	public void setUp() throws Exception {
+		fizzBuzzCalculator = new FizzBuzzCalculator();
+	}
+
 	@Test
 	public void fizzBuzzOf4Is4() {
-		FizzBuzzCalculator fizzBuzzCalculator = new FizzBuzzCalculator();
 		assertThat(fizzBuzzCalculator.fizzBuzz(4), is(equalTo("4")));
+	}
+
+	@Test
+	public void fizzBuzzOf5IsBuzz() {
+		assertThat(fizzBuzzCalculator.fizzBuzz(5), is(equalTo("BUZZ")));
 	}
 
 }
