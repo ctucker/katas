887ca35f4a22d6ffecad30164747a4c116516f89
Chris Tucker <chris.tucker@service-now.com>
2013-06-01 15:47:07 -0700

    Refactor: extract fizzBuzz as a private method

--- fizzbuzz/src/main/java/com/cjtucker/Main.java
+++ fizzbuzz/src/main/java/com/cjtucker/Main.java
@@ -3,11 +3,16 @@ package com.cjtucker;
 public class Main {
 	public static void main(String[] strings) {
 		for (int i = 1; i <= 100; i++) {
-			String output = String.valueOf(i);
-			if (i == 3) {
-				output = "FIZZ";
-			}
+			String output = fizzBuzz(i);
 			System.out.println(output);
 		}
+	}
+
+	private static String fizzBuzz(int i) {
+		String output = String.valueOf(i);
+		if (i == 3) {
+			output = "FIZZ";
+		}
+		return output;
 	}
 }
