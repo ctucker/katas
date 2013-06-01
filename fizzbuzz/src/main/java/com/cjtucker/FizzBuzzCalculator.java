f68475312c33bde1b0f7247b51b7a5b1910b3ed2
Chris Tucker <chris.tucker@service-now.com>
2013-06-01 16:12:55 -0700

    Refactor: Extract FIZZ/BUZZ/FIZZBUZZ constants in FizzBuzzCalculator

--- fizzbuzz/src/main/java/com/cjtucker/FizzBuzzCalculator.java
+++ fizzbuzz/src/main/java/com/cjtucker/FizzBuzzCalculator.java
@@ -2,16 +2,20 @@ package com.cjtucker;
 
 public class FizzBuzzCalculator {
 
+	public static final String FIZZ = "FIZZ";
+	public static final String BUZZ = "BUZZ";
+	public static final String FIZZBUZZ = "FIZZBUZZ";
+
 	public String fizzBuzz(int i) {
 		String output = String.valueOf(i);
 		if (i % 3 == 0) {
-			output = "FIZZ";
+			output = FIZZ;
 		}
 		if (i % 5 == 0) {
-			output = "BUZZ";
+			output = BUZZ;
 		}
 		if (i % (3 * 5) == 0) {
-			output = "FIZZBUZZ";
+			output = FIZZBUZZ;
 		}
 		return output;
 	}
